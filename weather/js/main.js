00c71360c1351cd5fd38f1d9e2296f32f747bb1c
samdevbeltran <samigamedev96@gmail.com>
2022-06-28 20:05:52 -0500

    forecast and banner created

--- weather/js/main.js
+++ weather/js/main.js
@@ -2,6 +2,12 @@ function toggle_open(){
     document.getElementById("primaryNav").classList.toggle("open");
 }
 date_var = document.getElementsByTagName("footer")[0].getElementsByTagName("span")[0]
-date_var.innerHTML = new Date()
+d = new Date()
+currentDay = d.getDay()
+
+if(currentDay == 5){
+    document.querySelector(".fri-banner").classList.remove("hidden")    
+}
+date_var.innerHTML = d
 btn = document.getElementById("hamburgerBtn")
 btn.onclick = toggle_open
