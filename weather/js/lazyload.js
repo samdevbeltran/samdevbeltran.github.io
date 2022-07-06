2c95865dd12033ba8fba0cbd9ba1b73451d45001
samdevbeltran <samigamedev96@gmail.com>
2022-07-06 18:37:54 -0500

    gallery created

---
+++ weather/js/lazyload.js
@@ -0,0 +1,28 @@
+const images = document.querySelectorAll("[data-src]");
+
+function preLoadImg(img){
+    let src = img.getAttribute("data-src")
+    if(!src){
+        return;
+    }else{
+        img.src = src
+        img.removeAttribute("data-src")
+    }
+    
+}
+
+const intersectionObserver = new IntersectionObserver((entries,imgObserver) => {
+    entries.forEach(function(entry){
+        if(entry.isIntersecting){
+            preLoadImg(entry.target)
+            console.log(entry.target)
+            intersectionObserver.unobserve(entry.target)
+        }else{
+            return;
+        }
+    })
+});
+
+images.forEach((img) => {
+    intersectionObserver.observe(img)
+})
